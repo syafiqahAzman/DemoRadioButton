e456970a8b53afe27148b889c073d3b298ba0e35
16046491 <16046491@myrp.edu.sg>
2020-04-24 13:23:59 +0800

    Add radio button in a radio group, then button below it. Add code to allow toast to appear when select on one radio button and it will appear after pressing the okay button

--- app/src/main/java/com/myapplicationdev/android/demoradiobutton/MainActivity.java
+++ app/src/main/java/com/myapplicationdev/android/demoradiobutton/MainActivity.java
@@ -3,6 +3,11 @@ package com.myapplicationdev.android.demoradiobutton;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn = (Button) findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View arg0) {
+                // Get the RadioGroup object
+                RadioGroup rg = (RadioGroup) findViewById(R.id.radioGroup1);
+                // Get the Id of the selected radio button in the RadioGroup
+                int selectedButtonId = rg.getCheckedRadioButtonId();
+                // Get the radio button object from the Id we had gotten above
+                RadioButton rb = (RadioButton) findViewById(selectedButtonId);
+                //Show a Toast that display the text on the selected radio button
+                Toast.makeText(MainActivity.this, rb.getText(),
+                        Toast.LENGTH_LONG).show();
+            }});
+
+
     }
 }
